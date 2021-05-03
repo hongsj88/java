6481e5728018997b2ad96b1e626a33ac69d82ffc
hongsj88 <43080832+hongsj88@users.noreply.github.com>
2021-05-04 06:53:41 +0900

    Update Program.java

--- CollectionPrj/src/com/newlecture/app/util/Program.java
+++ CollectionPrj/src/com/newlecture/app/util/Program.java
@@ -3,19 +3,33 @@ package com.newlecture.app.util;
 public class Program {
 
 	public static void main(String[] args) {
-		GList<String> list = new GList();
+		GList<Integer> list = new GList();
 		list.add(3);
-		list.add(5);		
-		int size = list.size();
-		System.out.printf("size : %d\n",size);
-		
-		list.clear();
-		size = list.size();
-		System.out.printf("size : %d\n",size);
-		
+		list.add(5);	
+		list.add(6);
 		list.add(7);
-		Object num = list.get(0);
-		System.out.printf("num : %d\n",num);
+		
+		list.add(3);
+		list.add(5);	
+		list.add(6);
+		list.add(7);
+		
+		list.add(3);
+		list.add(5);	
+		list.add(6);
+		list.add(7);
+		for(int i=0;i<list.size();i++)
+			System.out.printf("%d\n",list.get(i));
+//		int size = list.size();
+//		System.out.printf("size : %d\n",size);
+//		
+//		list.clear();
+//		size = list.size();
+//		System.out.printf("size : %d\n",size);
+//		
+//		list.add(7);
+//		Object num = list.get(0);
+//		System.out.printf("num : %d\n",num);
 //		IntList list = new IntList();
 //		list.add(3);
 //		list.add(5);
